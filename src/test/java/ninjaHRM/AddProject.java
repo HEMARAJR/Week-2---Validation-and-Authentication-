bc0fbe28ec7c50a9c008466335ff90720f718b01
HEMARAJR <146846732+HEMARAJR@users.noreply.github.com>
2026-04-09 10:17:20 +0530

    Update project details in AddProject test

--- src/test/java/ninjaHRM/AddProject.java
+++ src/test/java/ninjaHRM/AddProject.java
@@ -10,8 +10,8 @@ public class AddProject {
 		.contentType("application/xml")
         .body("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n"
         		+ "<Project>\r\n"
-        		+ "	<createdBy>MoitheGreat</createdBy>\r\n"
-        		+ "	<projectName>test321</projectName>\r\n"
+        		+ "	<createdBy>Tester</createdBy>\r\n"
+        		+ "	<projectName>API endpoint testing</projectName>\r\n"
         		+ "	<status>Completed</status>\r\n"
         		+ "	<teamSize>0</teamSize>\r\n"
         		+ "</Project>")
