b8cbb9a7f22c3b0bc18cf122e1f60e3fff4cfbf6
HEMARAJR <146846732+HEMARAJR@users.noreply.github.com>
2026-04-09 09:48:16 +0530

    Update employee creation payload in CreateEmployee

--- src/test/java/dummyRestApi/CreateEmployee.java
+++ src/test/java/dummyRestApi/CreateEmployee.java
@@ -7,7 +7,7 @@ public class CreateEmployee {
 	public static void main(String[] args) {
 		RestAssured.given()
 		.contentType("application/json")
-		.body("{\"name\":\"test\",\"salary\":\"123\",\"age\":\"23\"}")
+		.body("{\"name\":\"test\",\"salary\":\"120000\",\"age\":\"21\"}")
 		
 		.when()
 			.post("https://dummy.restapiexample.com/api/v1/create")
